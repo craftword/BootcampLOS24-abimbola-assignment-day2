e56dff97265a8e0b69a0e60924a0b6e3edd1b697
Abimbola Olaitan <craftwordltd@gmail.com>
2017-07-04 16:17:24 +0100

    modify spec file

--- spec/oop-spec.js
+++ spec/oop-spec.js
@@ -13,10 +13,9 @@ describe("Test for OOP property", function() {
       it('should inherit functionality from Parent', function(){
       //create instance
       let child = new app.Samsung("IMEI167256363", "Andriod", 3);
-      
       // check the child can access the attribute of parent
       child._IMEICode.should.equal('IMEI167256363');
-      child._OS.shouild.equal('Andriod');
+      child._OS.should.equal('Andriod');
       
      });
 
@@ -26,16 +25,16 @@ describe("Test for OOP property", function() {
 
       // check the child can access the attribute of parent
      // child.Dial().to.be('Choice the network you want to use to call');
-       child._OS.shouild.equal('Andriod');
+       child._OS.should.equal('Andriod');
       
      });
 
-     it('parent should to access it own function and attributes ', function(){
+     it('parent should be able to access it own function and attributes ', function(){
       //create instance
       let parent = new app.MobilePhones("IMEI167256363", "Andriod", 3);
 
       // check the parent access its attribute
-      parent._OS.should.equal('Andriod');
+       parent._OS.should.equal('Andriod');
      // parent.Receive().should.eqaul('Receiving a call');
       
      });
